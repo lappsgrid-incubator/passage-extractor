f7dc93a8d7d0b1c59f400d607b8e13fa34465ed3
Keith Suderman <suderman@cs.vassar.edu>
2016-08-05 12:37:31 -0500

    Initial project setup

---
+++ src/main/groovy/org/anc/lapps/chunk/PassageExtractor.groovy
@@ -0,0 +1,237 @@
+package org.anc.lapps.chunk
+
+import com.fasterxml.jackson.databind.JsonMappingException
+import org.lappsgrid.api.WebService
+import org.lappsgrid.metadata.ServiceMetadata
+import org.lappsgrid.metadata.ServiceMetadataBuilder
+import org.lappsgrid.serialization.Data
+import org.lappsgrid.serialization.Serializer
+import org.lappsgrid.serialization.lif.Annotation
+import org.lappsgrid.serialization.lif.Container
+import org.lappsgrid.serialization.lif.View
+
+import static org.lappsgrid.discriminator.Discriminators.Uri
+
+/**
+ * @author Keith Suderman
+ */
+class PassageExtractor implements WebService {
+
+    static final String NO_INPUT = "No input was provided."
+    static final String INVALID_DISCRIMINATOR = "Invalid discriminator. Expected a LIF document but found "
+    static final String NO_PARAMETERS = "No input parameter map found."
+    static final String NO_PASSAGE_ANNOTAION = "No passage annotation type was provided."
+    static final String NO_ANNOTATIONS = "No view contains the selected annotation type."
+    static final String NO_KEYWORD = "No keyword provided."
+
+    String metadata
+
+    /**
+     * Entry point for a Lappsgrid service.
+     * <p>
+     * Each service on the Lappsgrid will accept {@code org.lappsgrid.serialization.Data} object
+     * and return a {@code Data} object with a {@code org.lappsgrid.serialization.lif.Container}
+     * payload.
+     * <p>
+     * Errors and exceptions the occur during processing should be wrapped in a {@code Data}
+     * object with the discriminator set to http://vocab.lappsgrid.org/ns/error
+     * <p>
+     * See <a href="https://lapp.github.io/org.lappsgrid.serialization/index.html?org/lappsgrid/serialization/Data.html>org.lappsgrid.serialization.Data</a><br />
+     * See <a href="https://lapp.github.io/org.lappsgrid.serialization/index.html?org/lappsgrid/serialization/lif/Container.html>org.lappsgrid.serialization.lif.Container</a><br />
+     *
+     *
+     * @param input A JSON string representing a Data object
+     * @return A JSON string containing a Data object with a Container payload.
+     */
+    @Override
+    String execute(String input) {
+        if (input == null) {
+            return error(NO_INPUT)
+        }
+        Data data
+        try {
+            data = Serializer.parse(input, Data)
+        }
+        catch (JsonMappingException e) {
+            return error(e.message)
+        }
+
+        if (Uri.ERROR == data.discriminator) {
+            return input
+        }
+        if (Uri.LIF != data.discriminator && Uri.LAPPS != data.discriminator) {
+            return error(INVALID_DISCRIMINATOR + data.discriminator)
+        }
+        if (data.parameters == null) {
+            return error(NO_PARAMETERS)
+        }
+        String annotationType = data.parameters.annotation
+        if (annotationType == null) {
+            return error(NO_PASSAGE_ANNOTAION)
+        }
+        String keyword = data.parameters.keyword
+        if (keyword == null) {
+            return error(NO_KEYWORD)
+        }
+
+        Container container = new Container(data.payload)
+        List<View> views = container.findViewsThatContain(annotationType)
+        if (views == null || views.size() == 0) {
+            Container result = new Container()
+            result.text = ''
+            result.metadata = container.metadata
+            View view = result.newView()
+            view.addContains(annotationType, PassageExtractor.class.name, annotationType)
+            return new Data(Uri.LIF, result).asJson()
+//            return error(NO_ANNOTATIONS)
+        }
+
+        String text = container.text
+
+        // Buffer used to assemble the output (result) text.
+        StringBuilder buffer = new StringBuilder()
+        int offset = 0
+        int id = 0
+
+        Container resultContainer = new Container()
+        View resultView = resultContainer.newView()
+        resultView.addContains(annotationType, PassageExtractor.class.name, annotationType)
+
+        // Get the last view and iterate over each annotation and find each
+        // span that contains the keyword.
+        View view = views[-1]
+        view.annotations.each { Annotation a ->
+            String covered = text.substring((int)a.start, (int)a.end)
+            if (covered.contains(keyword)) {
+                resultView.newAnnotation("kw=${++id}", annotationType, offset, offset + covered.length())
+                buffer.append(covered)
+                buffer.append('\n')
+                offset = buffer.length()
+            }
+        }
+        resultContainer.text = buffer.toString()
+        return new Data(Uri.LIF, resultContainer).asPrettyJson()
+    }
+
+    /**
+     * Returns a JSON string containing metadata describing the service. The
+     * JSON <em>must</em> conform to the json-schema at
+     * <a href="http://vocab.lappsgrid.org/schema/service-schema.json">http://vocab.lappsgrid.org/schema/service-schema.json</a>
+     * (processing services) or
+     * <a href="http://vocab.lappsgrid.org/schema/datasource-schema.json">http://vocab.lappsgrid.org/schema/datasource-schema.json</a>
+     * (datasources).
+     *
+     */
+    @Override
+    String getMetadata() {
+        if (!metadata) {
+            ServiceMetadata md = new ServiceMetadataBuilder()
+                .allow(Uri.ANY)
+                .license(Uri.APACHE2)
+                .vendor("http://www.anc.org")
+                .name(PassageExtractor.class.name)
+                .version(Version.getVersion())
+                .description('Extracts all segments (chunks) that contain the given string(s).')
+                .requireEncoding('UTF-8')
+                .requireFormat(Uri.LIF)
+                .produceEncoding('UTF-8')
+                .produceFormat(Uri.LIF)
+                .build()
+            metadata = Serializer.toPrettyJson(md)
+        }
+        return metadata
+    }
+
+    private String error(String message) {
+        return new Data(Uri.ERROR, message).asPrettyJson()
+    }
+
+
+    public static void main(String[] args) {
+        CliBuilder cli = new CliBuilder()
+        cli.usage = "pe -a <annotation> -k <keyword> -o <path>"
+        cli.header = '''\nOptions:
+
+'''
+//        cli.header = '''
+//NAME
+//    passage extractor
+//
+//DESCRIPTION
+//    Extracts all passages from the text that contain the given
+//    keyword.  Passages are defined by the annotation type given.
+//
+//PARAMETERS
+//
+//'''
+        cli.footer = "\nCopyright 2016 American National Corpus. All rights reserved."
+        cli.a(longOpt:'annotation', args:1, argName: 'uri', 'annotation type that defines the passage to be extracted.')
+        cli.k(longOpt:'keyword', args:1, argName: 'string', 'passages containing this keyword will be extracted')
+        cli.o(longOpt:'output', args:1, argName: 'file', 'output file that will be created.')
+        cli.v(longOpt:'version', 'current version of the program')
+        cli.h(longOpt:'help', 'prints this help message')
+        cli.i(longOpt:'input', args:1, argName: 'file', 'the input file to be loaded.' )
+
+//        cli.usage()
+        def params = cli.parse(args)
+        if (!params) {
+            println "Unable to parse the command line parameters"
+            cli.usage()
+            return
+        }
+
+        if (params.h) {
+            cli.usage()
+            return
+        }
+
+        if (params.v) {
+//            char c = 0xA9
+            println()
+            println "Lappsgrid Passage Extractor v${Version.getVersion()}"
+            println "Copyright 2016 American National Corpus. All rights reserved."
+            println()
+            return
+        }
+
+        String annotation = params.a
+        String keyword = params.k
+        String input = params.i
+        String output = params.o
+
+        List<String> errors = new ArrayList<>()
+        if (!annotation) {
+            errors << 'No annotation specified.'
+        }
+        if (!keyword) {
+            errors << 'No keyword specified.'
+        }
+        File inputFile = new File(input)
+        if (!inputFile.exists()) {
+            errors << "Input file not found: ${inputFile.path}"
+        }
+
+        if (errors.size() > 0) {
+            println "One or more errors were encountered: "
+            errors.eachWithIndex{ String message, int i ->
+                println "${i}: ${message}"
+            }
+            return
+        }
+
+
+        Data data = new Data()
+        data.discriminator = Uri.LIF
+        data.parameters = [
+                annotation: annotation,
+                keyword: keyword
+        ]
+        Container container = new Container()
+        container.text = inputFile.text
+        data.payload = container
+
+        String json = new PassageExtractor().execute(data.asJson())
+        println json
+
+    }
+}
---
+++ src/test/groovy/org/anc/lapps/chunk/PassageExtractorTest.groovy
@@ -0,0 +1,206 @@
+package org.anc.lapps.chunk
+
+import org.junit.*
+import org.lappsgrid.api.WebService
+import org.lappsgrid.metadata.ServiceMetadata
+import org.lappsgrid.serialization.Data
+import org.lappsgrid.serialization.Serializer
+import org.lappsgrid.serialization.lif.Annotation
+import org.lappsgrid.serialization.lif.Container
+import org.lappsgrid.serialization.lif.View
+
+import static org.lappsgrid.discriminator.Discriminators.Uri
+import static org.junit.Assert.*
+
+/**
+ * @author Keith Suderman
+ */
+class PassageExtractorTest {
+    static final String S1 = "Barak Obama is the 44th president of the United States."
+    static final String S2 = "London is the capital city of England."
+    static final String S3 = "Queen Elizabeth is the monarch of England."
+    static final String UTF8 = 'UTF-8'
+
+    WebService service
+    Container container
+
+    @Before
+    void setup() {
+        service = new PassageExtractor()
+    }
+
+    @After
+    void teardown() {
+        service = null
+    }
+
+    @Test
+    void testMetadata() {
+        ServiceMetadata metadata = Serializer.parse(service.getMetadata(), ServiceMetadata)
+        assertNotNull metadata
+        assertEquals(Uri.ANY, metadata.allow)
+        assertEquals(Uri.APACHE2, metadata.license)
+        assertEquals(PassageExtractor.class.name, metadata.name)
+        assertEquals(Version.getVersion(), metadata.version)
+        assertEquals("http://www.anc.org", metadata.vendor)
+        assertEquals(UTF8, metadata.requires.encoding)
+        assertEquals(UTF8, metadata.produces.encoding)
+        assertEquals(1, metadata.requires.format.size())
+        assertEquals(1, metadata.produces.format.size())
+        assertEquals(Uri.LIF, metadata.requires.format[0])
+        assertEquals(Uri.LIF, metadata.produces.format[0])
+    }
+
+    @Test
+    void testNoInput() {
+        String json = service.execute()
+        assertNotNull json
+        Data data = Serializer.parse(json, Data)
+        hasError(data)
+        assertEquals(PassageExtractor.NO_INPUT, data.payload.toString())
+    }
+
+    @Test
+    void testEmptyInput() {
+        String json = service.execute('')
+        assertNotNull json
+        Data data = Serializer.parse(json, Data)
+        hasError(data)
+        assertTrue(data.payload.toString().startsWith('No content'))
+    }
+
+    @Test
+    void testError() {
+        String expected = 'error message'
+        Data data = new Data(Uri.ERROR, expected)
+        data = execute(data)
+        hasError(data)
+        assertEquals(expected, data.payload.toString())
+    }
+
+    @Test
+    void testWrongFormat() {
+        Data data = new Data(Uri.TEXT, 'hello world')
+        data = execute(data)
+        hasError(data)
+        assertTrue(data.payload.toString().startsWith(PassageExtractor.INVALID_DISCRIMINATOR))
+    }
+
+    @Test
+    void testNoParameters() {
+        Data data = new Data(Uri.LIF, null)
+        data = execute(data)
+        hasError(data)
+        assertEquals(PassageExtractor.NO_PARAMETERS, data.payload.toString())
+    }
+
+    @Test
+    void testNoSegment() {
+        Data data = new Data(Uri.LIF, null)
+        data.parameters = [:]
+        data = execute(data)
+        hasError(data)
+        assertEquals(PassageExtractor.NO_PASSAGE_ANNOTAION, data.payload.toString())
+    }
+
+    @Test
+    void testNoKeyword() {
+        Data data = new Data(Uri.LIF, null)
+        data.parameters = [ annotation: Uri.TOKEN ]
+        data = execute(data)
+        hasError(data)
+        assertEquals(PassageExtractor.NO_KEYWORD, data.payload.toString())
+    }
+
+    @Test
+    void testNoAnnotations() {
+        Data data = makeData()
+        data.parameters = [
+                keyword: 'foobar',
+                annotation: 'token'
+        ]
+        data = execute(data)
+        assertEquals(Uri.LIF, data.discriminator)
+        container = new Container(data.payload)
+        assertNotNull container.text
+        println "'${container.text}'"
+        assertEquals('', container.text)
+        assertEquals(1, container.views.size())
+        View view = container.views[0]
+        assertEquals(0, view.annotations.size())
+        assertTrue(view.contains('token'))
+//        hasError(data)
+//        assertEquals(PassageExtractor.NO_ANNOTATIONS, data.payload.toString())
+    }
+
+    @Test
+    void testChunkExtractor() {
+        Data original = makeData()
+        original.parameters = [
+                keyword: 'Barak Obama',
+                annotation: Uri.SENTENCE
+        ]
+        Data data = execute(original)
+        assertEquals(Uri.LIF, data.discriminator)
+        container = new Container(data.payload)
+        List<View> views = container.findViewsThatContain(Uri.SENTENCE)
+        assertNotNull(views)
+        assertEquals(1, views.size())
+        List<Annotation> annotations = views[0].annotations
+        assertNotNull(annotations)
+        assertEquals(1, annotations.size())
+        Annotation annotation = annotations[0]
+        assertEquals(Uri.SENTENCE, annotation.atType)
+        String text = container.text.substring((int)annotation.start, (int)annotation.end)
+        assertEquals(S1, text)
+
+        original.parameters.keyword = 'London'
+        data = execute(original)
+        container = new Container(data.payload)
+        views = container.findViewsThatContain(Uri.SENTENCE)
+        assertEquals(1, views.size())
+        annotations = views[0].annotations
+        assertEquals(1, annotations.size())
+        Annotation a = annotations[0]
+        assertEquals(S2, getText(a))
+
+        original.parameters.keyword = 'England'
+        data = execute(original)
+        container = new Container(data.payload)
+        views = container.findViewsThatContain(Uri.SENTENCE)
+        assertEquals(1, views.size())
+        annotations = views[0].annotations
+        assertEquals(2, annotations.size())
+        assertEquals(S2, getText((Annotation)annotations[0]))
+        assertEquals(S3, getText((Annotation)annotations[1]))
+    }
+
+    String getText(Annotation annotation) {
+        return container.text.substring((int)annotation.start, (int)annotation.end)
+    }
+
+    Data execute(Data data) {
+        return Serializer.parse(service.execute(data.asJson()), Data)
+    }
+    void hasError(Data data) {
+        assertEquals(Uri.ERROR, data.discriminator)
+    }
+
+    Data makeData() {
+        Container container = new Container()
+        View view = container.newView()
+        view.addContains(Uri.SENTENCE, "PassageExtractor Test Case", "sentence")
+
+        StringBuilder buffer = new StringBuilder()
+        int offset = 0
+        int id = 0
+        [S1, S2, S3].each { String sentence ->
+            view.newAnnotation("s=${++id}", Uri.SENTENCE, offset, offset + sentence.length())
+            buffer.append(sentence)
+            buffer.append('\n')
+            offset = buffer.length()
+        }
+        container.text = buffer.toString()
+        return new Data(Uri.LIF, container)
+    }
+}
